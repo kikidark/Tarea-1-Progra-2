8e9967e203bc1b8ecb2e577886109cc2b0b2cc52
kikidark <kikidark@users.noreply.github.com>
2020-03-05 07:43:43 -0600

    update tarea1

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,29 @@
 #include<stdio.h>	
 #include <iostream>
+#include <time.h>
 
 int main() {
 
-	int secret = rand() % 9+1,num=0;
+	srand(time(NULL));
+
+	int secret = rand() % 9+1 ,num=0;
 	std::cout << "El Numero Secreto es :" << secret << "\n";
 
 	while (num!=secret) {
 
 		std::cout << "Inserte numero de 1 al 10 : \n";
 		std::cin >> num;
+
+		if (num<secret) {
+			std::cout << "El Numero Secreto Es Mayor \n";
+		}
+		if (num > secret) {
+			std::cout << "El Numero Secreto Es Menor \n";
+		}
 	
 	}
 
 	std::cout << "Numero Correcto!!!\n";
-
-	
-
 
 	return 0;
 }
